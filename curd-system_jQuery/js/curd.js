d45e54becab8547f6f938b7c9849178396ef0588
chung <xiaozhou.cd@gmail.com>
2020-10-01 23:10:13 +0800

    style:把确定功能改成jQuery写法

--- curd-system_jQuery/js/curd.js
+++ curd-system_jQuery/js/curd.js
@@ -8,19 +8,19 @@ $('#tbody-shop').on('click', 'button', function () {
       $(this).next().val($(this).html())
     })
     // 确定按钮
-  } else if (curdBtn.includes('btn-ok')) {
-    const id = $tr.getAt$tribute('data-id'),
-      name = $tr.querySelector('.input-name').value,
-      price = $tr.querySelector('.input-price').value,
-      num = $tr.querySelector('.input-num').value
+  } else if ($(this).includes('btn-ok')) {
+    const id = $tr.data('id'),
+      name = $tr('.input-name').val(),
+      price = $tr('.input-price').val(),
+      num = $tr('.input-num').val()
     // 向后端发送请求
-    utils.fetch('./api/shop/edit.php', { id, name, price, num }).then(resp => {
+    $.get('./api/shop/edit.php', { id, name, price, num }).then(resp => {
       if (resp.code === 200) {
-        $tr.classList.remove('edit')
-        const spans = $tr.querySelectorAll('span')
-        spans.forEach(span => {
-          span.innerHTML = span.nextElementSibling.value
+        $tr.removeClass('edit').find('span').each(function () {
+          $(this).html($(this).next().val())
         })
+      } else {
+        alert('网络错误,数据修改失败')
       }
     })
     // 删除按钮

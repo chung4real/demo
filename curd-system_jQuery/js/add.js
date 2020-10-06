3909e9eb7acef68cb718d3ee8855c1fe43ac5228
chung <xiaozhou.cd@gmail.com>
2020-10-06 21:41:03 +0800

    style:把添加之后清除添加框的输入值改成jQuery写法

--- curd-system_jQuery/js/add.js
+++ curd-system_jQuery/js/add.js
@@ -1,8 +1,8 @@
 // 给添加按钮写点击事件
 $('#btn-add').on('click', function () {
   const name = $('#inputNum').val(),
-    price = $('#inputName').val(),
-    num = $('#inputPrice').val()
+        price = $('#inputName').val(),
+        num = $('#inputPrice').val()
   $.get('./api/shop/add.php', { name, price, num }, function (resp) {
     if (resp.code === 200) {
       $('#addModal').modal('hide')
@@ -17,5 +17,7 @@ $('#btn-add').on('click', function () {
 })
 // 添加之后清除添加框的输入值
 $('#addModal').on('hidden.bs.modal', function () {
-  inputName.value = inputPrice.value = inputNum.value = ''
+  $('#inputName').val('')
+  $('#inputPrice').val('')
+  $('#inputNum').val('')
 })
---
+++ curd-system_jQuery/js/dist/add.dev.js
@@ -0,0 +1,29 @@
+"use strict";
+
+// 给添加按钮写点击事件
+$('#btn-add').on('click', function () {
+  var name = $('#inputNum').val(),
+      price = $('#inputName').val(),
+      num = $('#inputPrice').val();
+  $.get('./api/shop/add.php', {
+    name: name,
+    price: price,
+    num: num
+  }, function (resp) {
+    if (resp.code === 200) {
+      $('#addModal').modal('hide');
+      getData();
+    } else {
+      $('#addFail').removeClass('hide');
+      setTimeout(function () {
+        $('#addFail').addClass('hide');
+      }, 2000);
+    }
+  }, 'json');
+}); // 添加之后清除添加框的输入值
+
+$('#addModal').on('hidden.bs.modal', function () {
+  $('#inputName').val('');
+  $('#inputPrice').val('');
+  $('#inputNum').val('');
+});
